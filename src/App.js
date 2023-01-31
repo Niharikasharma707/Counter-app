a0e6a293d053d5dd0be054a2ead59494d4c28b92
Niharikasharma707 <niharikasharma9640@gmail.com>
2023-01-31 11:50:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Header } from 'semantic-ui-react';
+import {Counter} from './component/Counter.jsx'
 
 function App() {
+  const[count,setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{display: "flex",textAlign: "center",justifyContent:"center",alignItems: "center",minHeight:"80vh"}}>
+    <Counter count={count} setCount={setCount}></Counter>
     </div>
+    <Header textAlign='center' as="h3">Done by - Niharika Sharma- 12005467</Header>
+    </>
   );
 }
 
